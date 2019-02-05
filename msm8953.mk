fdb7e5a0b09c667973241cdcf3bc23d40a438e42
saksham033 <sakshamgoyal02@gmail.com>
2019-02-05 15:42:28 +0000

    daisy: update light service
    Signed-off-by: saksham033 <sakshamgoyal02@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -356,7 +356,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
+    android.hardware.thermal@1.0-service \
     android.hardware.usb@1.0-service.basic
 
 # Vibrator
