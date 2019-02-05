76a7f5a73b2ce89b70df72d311fc4e7b51f4f40a
saksham033 <sakshamgoyal02@gmail.com>
2019-02-05 13:40:47 +0000

    daisy: use overlay-lineage
    Signed-off-by: saksham033 <sakshamgoyal02@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -20,6 +20,7 @@ $(call inherit-product, vendor/xiaomi/msm8953-common/msm8953-common-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
