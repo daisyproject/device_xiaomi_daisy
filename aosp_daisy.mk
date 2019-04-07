af252f2a4cd787317077c68b94edc5e00d6c9dcf
saksham033 <sakshamgoyal02@gmail.com>
2019-04-08 02:54:11 +0530

    daisy: PE-fy
    PE is love ;-)
    
    Signed-off-by: saksham033 <sakshamgoyal02@gmail.com>

--- aosp_daisy.mk
+++ aosp_daisy.mk
@@ -7,10 +7,14 @@
 $(call inherit-product, device/xiaomi/daisy/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# PE stuff
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_daisy
+PRODUCT_NAME := aosp_daisy
 PRODUCT_DEVICE := daisy
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A2 Lite
